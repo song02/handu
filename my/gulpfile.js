5ae3c80c6fa7bc2a4067ea6bb2d6746f8db3758d
song02 <1692201941@qq.com>
2018-06-01 12:01:13 +0800

    完成新品上市部分

---
+++ my/gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+gulp.task('sass',function(){
+	gulp.src('src/scss/*.scss').pipe(sass()).pipe(rename({"suffix":".min"})).pipe(cssnano()).pipe(gulp.dest('css'));
+});
+
+gulp.task('js',function(){
+	gulp.src('src/js/*.js').pipe(uglify()).pipe(rename({"suffix":".min"})).pipe(concat("index.min.js")).pipe(gulp.dest('js'));
+});
+
+gulp.task('watch',function(){
+	gulp.watch(['src/scss/*.scss','src/js/*.js'],['sass','js']);
+});
